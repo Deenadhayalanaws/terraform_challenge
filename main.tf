80d9de81e44210e875c1ca08162f661cc244432c
Deenadhayalanaws <90705233+Deenadhayalanaws@users.noreply.github.com>
2024-09-17 13:05:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,38 +8,31 @@ resource "aws_security_group" "ec2_sg" {
   description = "Allow SSH and HTTP inbound traffic"
   vpc_id      = var.vpc_id
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Correct plural form
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Correct plural form
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Correct plural form
+  }
+
   tags = {
     Name = "ec2_sg"
   }
 }
-
-# Ingress rule for SSH (port 22)
-resource "aws_vpc_security_group_ingress_rule" "ssh_ingress" {
-  security_group_id = aws_security_group.ec2_sg.id
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-# Ingress rule for HTTP (port 80)
-resource "aws_vpc_security_group_ingress_rule" "http_ingress" {
-  security_group_id = aws_security_group.ec2_sg.id
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-# Egress rule for EC2
-resource "aws_vpc_security_group_egress_rule" "ec2_egress" {
-  security_group_id = aws_security_group.ec2_sg.id
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
 # EC2 Instance
 resource "aws_instance" "ec2_instance" {
   ami           = var.ami_id
