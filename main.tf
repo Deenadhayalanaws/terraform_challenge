cc27313d8e1a6d5c6ef9b48aee97bcf8d5d4e258
Deenadhayalanaws <90705233+Deenadhayalanaws@users.noreply.github.com>
2024-09-17 13:10:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Correct plural form
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   ingress {
